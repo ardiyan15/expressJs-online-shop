3323d54512b8717166a451d5c3e6dc6ccdaca7d5
ardiyan15 <ardhiyan15@gmail.com>
2021-02-16 16:25:34 +0700

    update

--- app.js
+++ app.js
@@ -1,58 +1,64 @@
-const path = require('path')
-const express = require('express')
-const bodyParser = require('body-parser')
-const mongoose = require('mongoose')
-const app = express()
+const path = require('path');
 
-app.set('view engine', 'ejs')
-app.set('views', 'views')
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
 
-const adminRoutes = require('./routes/admin')
-const shopRoutes = require('./routes/shop')
-const authRoutes = require('./routes/auth')
-const errorController = require('./controllers/error')
+const errorController = require('./controllers/error');
+const User = require('./models/user');
 
-const User = require('./models/user')
+const MONGODB_URI =
+  'mongodb+srv://ardiyan:Hiro@)@!@cluster0.hzc5z.mongodb.net/shop?retryWrites=true&w=majority';
 
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(express.static(path.join(__dirname, 'public')))
+const app = express();
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: 'sessions'
+});
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+  session({
+    secret: 'my secret',
+    resave: false,
+    saveUninitialized: false,
+    store: store
+  })
+);
 
 app.use((req, res, next) => {
-    User.findById('600ec7e291807614a4be28e9')
-        .then(user => {
-            req.user = user
-            next()
-        })
-        .catch(err => {
-            console.log(err)
-        })
-})
-
-app.use(authRoutes)
-app.use('/admin', adminRoutes)
-app.use(shopRoutes)
-
-app.use(errorController.get404)
-
-mongoose.connect(`mongodb+srv://ardiyan:Hiro@)@!@cluster0.hzc5z.mongodb.net/shop?retryWrites=true&w=majority`, {
-    useNewUrlParser: true, useUnifiedTopology: true
-})
-    .then(result => {
-        User.findOne().then(user => {
-            if(!user){
-                const user = new User({
-                    name: 'Ardiyan Agus',
-                    email: 'ardiyan@gmail.com',
-                    cart: {
-                        items: []
-                    }
-                })
-                user.save()
-            }
-        })
-        console.log('Connected!')
-        app.listen(3000)
+  if (!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
+    .then(user => {
+      req.user = user;
+      next();
     })
-    .catch(err => {
-        console.log(err)
-    })
+    .catch(err => console.log(err));
+});
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+app.use(authRoutes);
+
+app.use(errorController.get404);
+
+mongoose
+  .connect(MONGODB_URI, {useNewUrlParser: true})
+  .then(result => {
+    app.listen(3000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
