6492d8026f5bd2ddd6afec9770531d41d8e042c5
ardiyan15 <ardhiyan15@gmail.com>
2021-04-20 16:29:22 +0700

    Storing Data in the Database

--- controllers/admin.js
+++ controllers/admin.js
@@ -14,11 +14,26 @@ exports.getAddProduct = (req, res, next) => {
 };
 
 exports.postAddProduct = (req, res, next) => {
-  const title = req.body.title;
-  const imageUrl = req.body.imageUrl;
-  const price = req.body.price;
+  const title       = req.body.title;
+  const image       = req.file;
+  const price       = req.body.price;
   const description = req.body.description;
-  const errors = validationResult(req)
+  if(!image){
+    return res.status(422).render('admin/edit-product', {
+      pageTitle: 'Add Product',
+      path: '/admin/edit-product',
+      editing: false,
+      hasError: true, 
+      product: {
+        title: title,
+        price: price,
+        description: description
+      },
+      errorMessage: 'Attached file is not an image.',
+      validationErrors: []
+    });
+  }
+  const errors      = validationResult(req)
 
   if(!errors.isEmpty()){
     return res.status(422).render('admin/edit-product', {
@@ -37,6 +52,8 @@ exports.postAddProduct = (req, res, next) => {
     });
   }
 
+  const imageUrl = image.path
+
   const product = new Product({
     _id: new mongoose.Types.ObjectId('600eccfda7d14734582f75ae'),
     title: title,
@@ -48,7 +65,6 @@ exports.postAddProduct = (req, res, next) => {
   product
     .save()
     .then(result => {
-      // console.log(result);
       console.log('Created Product');
       res.redirect('/admin/products');
     })
@@ -103,12 +119,12 @@ exports.getEditProduct = (req, res, next) => {
 };
 
 exports.postEditProduct = (req, res, next) => {
-  const prodId = req.body.productId;
-  const updatedTitle = req.body.title;
-  const updatedPrice = req.body.price;
-  const updatedImageUrl = req.body.imageUrl;
-  const updatedDesc = req.body.description;
-  const errors = validationResult(req)
+  const prodId          = req.body.productId;
+  const updatedTitle    = req.body.title;
+  const updatedPrice    = req.body.price;
+  const image           = req.file;
+  const updatedDesc     = req.body.description;
+  const errors          = validationResult(req)
 
   if(!errors.isEmpty()){
     return res.status(422).render('admin/edit-product', {
@@ -118,7 +134,6 @@ exports.postEditProduct = (req, res, next) => {
       hasError: true, 
       product: {
         title: updatedTitle,
-        imageUrl: updatedImageUrl,
         price: updatedPrice,
         description: updatedDesc,
         _id: prodId
@@ -136,7 +151,9 @@ exports.postEditProduct = (req, res, next) => {
       product.title = updatedTitle;
       product.price = updatedPrice;
       product.description = updatedDesc;
-      product.imageUrl = updatedImageUrl;
+      if(image){
+        product.imageUrl = image.path;
+      }
       return product.save()
       .then(result => {
         console.log('UPDATED PRODUCT!');
